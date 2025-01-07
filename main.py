3f7973eb1f68f1f5bfdc81a7fba15d891f999d5b
Mr.Slime <none>
2025-01-07 02:38:00 +0200

    Опис змін

---
+++ main.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+chrome_options = Options()
+chrome_options.headless = True
+
+driver = webdriver.Chrome(options=chrome_options)
+
+try:
+    driver.get("https://nz.ua")
+    time.sleep(0.01)
+    page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source, "html.parser")
+    block = soup.find("div", class_="lp-wrapp")
+    
+    if block:
+        paragraph = block.find("p")
+        if paragraph:
+            print("Знайдений текст:", paragraph.text)
+        else:
+            print("Текстовий елемент <p> не знайдено.")
+    else:
+        print("Елемент з класом 'lp-wrapp' не знайдено.")
+finally:
+    driver.quit()
