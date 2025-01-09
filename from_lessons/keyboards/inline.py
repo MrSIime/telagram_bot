fe191d7915e40ba85729dd26c1f995704c29d4a1
Mr.Slime <none>
2025-01-09 15:25:50 +0200

    add code from lessons

---
+++ from_lessons/keyboards/inline.py
@@ -0,0 +1,13 @@
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+links = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="YouTube", url="https://youtu.be/@fsoky"),
+            InlineKeyboardButton(text="Telegram", url="tg://resolve?domain=fsoky_community")
+        ]
+    ]
+)
